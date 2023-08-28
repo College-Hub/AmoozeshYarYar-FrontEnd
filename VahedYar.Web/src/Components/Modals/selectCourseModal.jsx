8d8007a9d513fd24f5bd8297da63184948f7a7d7
sadEasterner <hoseinseifig1380@gmail.com>
2023-08-28 23:46:39 +0330

    [Add] : Added more Details to select course modal table

--- VahedYar.Web/src/Components/Modals/selectCourseModal.jsx
+++ VahedYar.Web/src/Components/Modals/selectCourseModal.jsx
@@ -7,6 +7,7 @@ import { courseActions } from "../../Store/course-slice";
 import { uiActions } from "../../Store/ui-slice";
 import { Fragment, useEffect, useState } from 'react';
 import LoadSpiner from '../Animations/loadSpiner';
+import { toPersianNumber } from '../../feratures/helper/helper';
 
 const SelectCourseModal = (prop) => {
     // state
@@ -87,12 +88,12 @@ const SelectCourseModal = (prop) => {
                     <Modal show={content === "COURSE"} fullscreen={true} onHide={closeHandler} size="xl" centered>
                         <Modal.Body className={"modal-course"}>
                             <div className="m-2 mt-3">
-                                <div className="select-course-Modal-info row" dir="ltr">
-                                    <div className="col-12" dir="rtl" >
+                                <div className="select-course-Modal-info row">
+                                    <div className="col-12">
                                         <h4><i className="bi bi-search"></i> فیلتر درس  </h4 >
                                     </div>
                                     {/*<div className="d-none d-lg-block col-lg-4"></div>*/}
-                                    <div className="col-12 col-md-6 col-lg-8 mb-4 pt-4" dir="rtl">
+                                    <div className="col-12 mb-4 pt-4" dir="rtl">
                                         <div className="">
                                             <span className="ms-2"><i className="bi bi-exclamation-circle"></i></span>
                                             <span>برای برداشتن درس های معارف راحت تر هستش که فیلتر گروه ارائه دهنده رو به گروه معارف تغییر بدی!</span>
@@ -102,63 +103,55 @@ const SelectCourseModal = (prop) => {
                                             <span>یادت باشه ما فقط داریم درس هایی رو بهت نشون میدیم که گروه درسی خودت ارائه میده! در صورتی که گروه درسیت رو اشتباه انتخاب کردی روی لینک تغییر مشخصات اولیه کلیک کن.</span>
                                         </div>
                                     </div>
-                                    <div className="col-12 col-md-6 col-lg-4" dir="rtl">
+                                    <div className="col-12" dir="rtl">
                                         <div className="select-course-Modal-search-row row">
-                                            <div className="col-12 mt-2">
+                                            <div className="col-12 col-lg-6 mt-2">
                                                 <label className="mb-2">عنوان درس :</label>
                                                 <div className="search-input-group mb-1">
                                                     <input className="form-control form-control-sm" type="text" placeholder="" id="filterTitleInput" onChange={filterTitleHandler} aria-label=".form-control-sm example" />
+                                                    <button className="custome-btn-danger">حذف</button>
                                                 </div>
                                             </div>
-                                            <div className="col-12 mt-2">
-                                                <label className="mb-2">گروه ارائه دهنده :</label>
-                                                <select className="form-select form-select-sm custome-selectInput-course-Modal " aria-label="Default select example">
-                                                    <option>همه</option>
-                                                </select>
-                                            </div>
-                                            <div className="col-12 mt-2">
-                                                <label className="mb-2">واحد درسی :</label>
-                                                <select className="form-select form-select-sm custome-selectInput-course-Modal " aria-label="Default select example">
-                                                    <option>همه</option>
-                                                </select>
-                                            </div>
-
                                         </div>
                                         <div className="col-12 mt-4">
-                                            <button className="custome-btn-danger">حذف فیلتر</button>
+                                            
                                         </div>
 
                                     </div>
-                                    <div className="col-4 d-flex justify-content-end" dir="rtl">
+                                    <div className="col-12 d-flex justify-content-end" dir="rtl">
                                         <button className="custome-btn-info" onClick={closeHandler}>بازگشت</button>
                                     </div>
 
                                 </div>
 
                                 <div className="select-course-Modal-table-header row">
-                                    <div className="d-none d-lg-block col-2 ">انتخاب <span className="d-none d-lg-inline">درس</span></div>
-                                    <div className="col-6 col-lg-3 text-center text-lg-end">عنوان <span className="d-none d-lg-inline">درس</span></div>
-                                    <div className="col-6 col-lg-3 text-end">گروه <span className="d-none d-lg-inline">ارائه دهنده</span></div>
-                                    <div className="d-none d-lg-block col-2 textcenter">واحد(نظری/عملی)</div>
-                                    <div className="col-1"></div>
+                                    <div className="d-none d-lg-block col-1 ">انتخاب <span className="d-none d-lg-inline">درس</span></div>
+                                    <div className="col-6 col-lg-3 text-end">عنوان <span className="d-none d-lg-inline">درس</span></div>
+                                    <div className="col-4 col-lg-2 text-end">گروه <span className="d-none d-lg-inline">ارائه دهنده</span></div>
+                                    <div className="d-none d-lg-block col-2 text-center">واحد(نظری/عملی)</div>
+                                    <div className="d-none d-lg-block col-2 text-center">ترم پیشنهادی</div>
+                                    <div className="col-2 text-center">جزئیات</div>
                                 </div>
                                 <div className="select-course-Modal-table-body">
                                     {
                                         page?.map(course => (
                                             <div key={course.courseId} className="row">
-                                                <div className="col-2  d-flex justify-content-center">
+                                                <div className="col-1  d-flex justify-content-center">
                                                     <input checked={checkForSeletedCourse(course.courseId)} className="form-check-input m-2" type="checkbox" id={course.courseId} onChange={selectCourseHandler} />
                                                 </div>
-                                                <div className="col-6 col-lg-3 ">
+                                                <div className="col-5 col-lg-3 ">
                                                     <span >{course.title}</span>
                                                 </div>
-                                                <div className="col-5 col-lg-3 ">
+                                                <div className="col-5 col-lg-2 ">
                                                     <span >{course.group}</span>
                                                 </div>
                                                 <div className="d-none d-lg-block col-2 text-center">
-                                                    <span >{course.theoreticalUnits}/{course.practicalUnits}</span>
+                                                    <span >{toPersianNumber(course.theoreticalUnits)}/{toPersianNumber(course.practicalUnits)}</span>
                                                 </div>
-                                                <div className="col-1 d-flex justify-content-center">
+                                                <div className="d-none d-lg-block col-2 d-flex justify-content-center">
+                                                    <span >{course.educationalLevel}</span>
+                                                </div>
+                                                <div className="col-2 d-flex justify-content-center">
                                                     <span >{course.unit}</span>
                                                 </div>
                                             </div>
@@ -168,11 +161,11 @@ const SelectCourseModal = (prop) => {
                                 <div className="select-course-Modal-table-footer row">
                                     <div className={"d-flex"} dir="ltr" >
                                         <div className="pagination">
-                                            <div className={amount > 1 ? "pagination-page" : "d-none"} data-name="1" onClick={paginationHandler}>{1}</div>
+                                            <div className={amount > 1 ? "pagination-page" : "d-none"} data-name="1" onClick={paginationHandler}>{toPersianNumber(1)}</div>
                                             <div className={(pageIndex < 1) ? "pagination-page disabled" : "pagination-page"} data-name="PREV" onClick={paginationHandler}>قبلی</div>
-                                            <div className={"pagination-page"} data-name={pageIndex}>{pageIndex + 1}</div>
+                                            <div className={"pagination-page"} data-name={pageIndex}>{toPersianNumber(pageIndex + 1)}</div>
                                             <div className={(pageIndex + 1 === amount) ? "pagination-page disabled" : "pagination-page"} data-name="NEXT" onClick={paginationHandler}>بعدی</div>
-                                            <div className={amount > 1 ? "pagination-page" : "d-none"} data-name={amount} onClick={paginationHandler}>{amount}</div>
+                                            <div className={amount > 1 ? "pagination-page" : "d-none"} data-name={amount} onClick={paginationHandler}>{toPersianNumber(amount)}</div>
                                         </div>
                                     </div>
 
