dc513fadb0fd51dd3813daa4b53ce65b44850d29
Hosein Seifi <hoseinseifig1380@gmail.com>
2023-09-01 16:25:44 +0330

    [Add] : Added mutations for user login and signup

--- VahedYar.Web/src/feratures/api/apiSlice.js
+++ VahedYar.Web/src/feratures/api/apiSlice.js
@@ -2,8 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
     reducerPath: 'api', // optional
-    
-
     baseQuery: fetchBaseQuery({
         //baseUrl: 'http://localhost:5269/api',
         baseUrl: 'https://localhost:7208/api',
@@ -14,7 +12,7 @@ export const apiSlice = createApi({
         //baseUrl:'http://5.160.82.66:11025/api',
     }),
     endpoints: builder => ({
-        //TODO: user authenticatoin should be added 
+
         getUniversiy: builder.query({
             query: () => ({
                 url: '/University/GetAll',
@@ -39,6 +37,26 @@ export const apiSlice = createApi({
                 },
                 body: JSON.stringify(filter),
             }) 
+        }),
+        login: builder.mutation({
+            query: (userInfo) => ({
+                url: `/Account/LogIn`,
+                method: 'POST',
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(userInfo),
+            })
+        }),
+        signup: builder.mutation({
+            query: (userInfo) => ({
+                url: `/Account/SignUp`,
+                method: 'POST',
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(userInfo),
+            })
         })
     }),
     keepUnusedDataor:  10 * 60 * 1000,
@@ -46,5 +64,7 @@ export const apiSlice = createApi({
 export const {
     useGetUniversiyQuery,
     useSubmitUserInfoMutation,
-    useSubmitFitersMutation
+    useSubmitFitersMutation,
+    useLoginMutation,
+    useSignupMutation,
 } = apiSlice
